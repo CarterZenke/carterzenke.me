642179186375bfde19cc9b912ba1c04c46075ee9
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-06-07 20:58:56 -0400

    update summer courses

--- homepage/views.py
+++ homepage/views.py
@@ -6,7 +6,7 @@ def index(request):
     spring_courses = Course.objects.filter(term="Spring", year=2022)
     winter_courses = Course.objects.filter(term="Winter", year=2022)
     fall_courses = Course.objects.filter(term="Fall", year=2021)
-    summer_courses = Course.objects.filter(term="Summer", year=2021)
+    summer_courses = Course.objects.filter(term="Summer", year=2022)
     return render(request, "homepage/index.html", {
         "spring_courses" : spring_courses,
         "winter_courses" : winter_courses,
