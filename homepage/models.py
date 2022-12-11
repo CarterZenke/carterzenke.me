f4ac7ad99affb9b7c328fbb74e8238d5603e22af
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-12-11 17:23:42 -0500

    improve schema for future courses

--- homepage/models.py
+++ homepage/models.py
@@ -5,10 +5,24 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=128)
     number = models.CharField(max_length=32)
-    term = models.CharField(max_length=16)
-    year = models.IntegerField()
+    term = models.ForeignKey(
+        'Term',
+        on_delete=models.CASCADE,
+    )
     school = models.CharField(max_length=64)
     role = models.CharField(max_length=32)
 
     def __str__(self):
-        return f"{self.id}: {self.number}, {self.term} {self.year}"
+        return f"{self.id}: {self.number}, {self.term}"
+
+
+class Term(models.Model):
+    semester = models.CharField(
+        max_length=6,
+        choices=[('1', 'Winter'), ('2', 'Spring'), ('3', 'Summer'), ('4', 'Fall')],
+        default='1'
+    )
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.semester} {self.year}"
--- homepage/views.py
+++ homepage/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-from .models import Course
+from .models import Course, Term
 
 # Create your views here.
 def index(request):
-    spring_courses = Course.objects.filter(term="Spring", year=2022)
-    winter_courses = Course.objects.filter(term="Winter", year=2022)
-    fall_courses = Course.objects.filter(term="Fall", year=2021)
-    summer_courses = Course.objects.filter(term="Summer", year=2022)
+    terms = Term.objects.all().order_by('-year', 'semester')
+
+    history = {}
+    for term in terms:
+        history[term] = Course.objects.filter(term=term)
+        for course in history[term]:
+            course.term.semester = course.term.get_semester_display()
+
     return render(request, "homepage/index.html", {
-        "spring_courses" : spring_courses,
-        "winter_courses" : winter_courses,
-        "fall_courses" : fall_courses,
-        "summer_courses" : summer_courses
+        "history" : history,
     })
