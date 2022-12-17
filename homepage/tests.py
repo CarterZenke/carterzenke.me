3d2215b70a68c1c57eaa2bff17f5d3af9752e37f
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2022-12-17 16:48:13 -0500

    add new tests

--- homepage/tests.py
+++ homepage/tests.py
@@ -23,14 +23,31 @@ class HomepageTestCase(TestCase):
         for term in TERMS:
             Term.objects.create(semester=term["semester"], year=term["year"])
 
+        self.response = self.client.get("/")
+
     def test_homepage_status(self):
         """Homepage returns 200"""
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_template(self):
+        """Homepage uses index.html and layout.html from homepage app"""
+        self.assertTemplateUsed(self.response, "homepage/index.html")
+        self.assertTemplateUsed(self.response, "homepage/layout.html")
+
+    def test_name_included(self):
+        """Name is included in homepage twice: once in meta title, once in title text"""
+        self.assertContains(self.response, "Carter Zenke", count=2)
+
+    def test_all_terms_listed_once(self):
+        """All terms appear exactly once in HTML"""
+        for term in TERMS:
+            self.assertContains(
+                self.response, f"{term['semester']} {term['year']}", count=1
+            )
 
     def test_terms_sorted(self):
-        response = self.client.get("/")
-        history = response.context["history"]
+        """Terms are listed most to least recent"""
+        history = self.response.context["history"]
         sorted_terms = sorted(TERMS, key=lambda term: (-term["year"], term["semester"]))
         for i, term in enumerate(history.keys()):
             self.assertEquals(sorted_terms[i]["semester"], term.semester)
